502fbfad652f4cf694232f007c98e144e3acbd29
Olatunji Tolulope Oluwafunmilayo <olatunjitolulope4@gmail.com>
2023-08-28 00:01:26 +0100

    added changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {BiMenuAltRight} from 'react-icons/bi'
 const Nav = () => {
   return (
-    <div className='flex flex-row justify-between m-10'>
+    <div className='flex flex-row justify-end md:justify-between m-10'>
       <h2 className='hidden md:flex'>Logo</h2>
       <div className='flex flex-row space-around gap-20 items-center'>
       <p className='text-[black] font-extrabold text-[24px] hidden md:flex'>Work</p>
       <p className='text-[black] font-extrabold text-[24px] hidden md:flex'>Contact</p>
-      <div className='flex flex-row justify-end text-right '>
-      <BiMenuAltRight className='text-[70px] text-right '/>
+      <div className='flex flex-row text-right'>
+      <BiMenuAltRight className='text-[70px] text-right'/>
       </div>
       </div>
     </div>
